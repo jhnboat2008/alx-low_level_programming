6f0220f7e1e28f35143a0c8ba8a7bb7d44cd07fe
jhnboat2008 <jhnboat2008@gmail.com>
2023-06-18 12:13:42 +0000

    Generates a random number and compares to a condition if true

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Generates a random number, compares to
+ * a condition and prints the last digit of the number
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n;
+	int x;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	x = n % 10;
+	printf("Last digit of %d is %d", n, x);
+	if (x > 5)
+		printf("and is greater than 5\n");
+	if (x == 0)
+		printf("and is 0\n");
+	if (x < 6 && x != 0)
+		printf("and is less than 6 and not 0\n");
+	return (0);
+}
