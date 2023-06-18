d80580dcf2939e82a2d8c84df8deb288c3b8998a
jhnboat2008 <jhnboat2008@gmail.com>
2023-06-18 13:58:42 +0000

    Prints all alphabets in lowercase except

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Prints all alphabets except q and e
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char lower;
+
+	for (lower = 'a'; lower <= 'z'; ++lower)
+	{
+		if (lower != 'e' && 'q')
+			putchar(lower);
+	}
+	putchar('\n');
+
+	return (0);
+}
