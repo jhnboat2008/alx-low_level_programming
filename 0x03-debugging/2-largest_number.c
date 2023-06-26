01fd2f786279ba06c4b17d718d8b2213ff902e8d
jhnboat2008 <jhnboat2008@gmail.com>
2023-06-26 20:12:09 +0000

    C program debugging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,10 +16,6 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (a > b && c > a)
-	{
-		largest = c;
-	}
 	else if (b > c)
 	{
 		largest = b;
@@ -29,5 +25,5 @@ int largest_number(int a, int b, int c)
 		largest = c;
 	}
 
-return (largest);
+	return (largest);
 }
