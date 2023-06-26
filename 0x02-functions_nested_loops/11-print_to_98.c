e0568a7d982ff13397e0712118bf53074b72ee3a
jhnboat2008 <jhnboat2008@gmail.com>
2023-06-26 18:45:39 +0000

    prints all natural numbers fron n to 98, separated by commas and followed by a space

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * with numbers separated by comma, followed by a space
+ * and sorted in order
+ * first printed number should be the number passed to the
+ * function
+ * @n: natural numbers
+*/
+
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++)
+		printf("%d\n", n);
+	}
+}
