5476da5f6a0034a7fecba73db2b409318bf6a58f
jhnboat2008 <jhnboat2008@gmail.com>
2023-06-27 12:23:45 +0000

    checks uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,13 +3,11 @@
 /**
  * _isupper - checks for uppercase characters
  * @c: uppercase character
- * Return: 1 if c is uppercase
+ * Return: 1 if c is uppercase or 0 otherwise
 */
 
 int _isupper(int c)
 {
-	int c;
-
 	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
